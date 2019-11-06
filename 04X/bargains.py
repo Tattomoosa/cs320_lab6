5326a56f4e3155b3e954734b4459212e801694f1
Matthew OTousa <mlotousa@gmail.com>
2019-11-06 01:11:36 -0800

    done (test more plz tho)

--- 04X/bargains.py
+++ 04X/bargains.py
@@ -15,23 +15,20 @@ def usage():
     exit(0)
 
 def main():
-    # try:
     bargains = []
-    filename = sys.argv[1]
+    try: filename = sys.argv[1]
+    except: usage()
     with open(filename) as file:
-        outfile = f'{filename.split(".")[0]}.bargains'
         next(file)
-        products = [
-                [*d, int(d[QUANTITY]) * float(d[PRICE])]
-                for d in (line[:-1].split(',') for line in file)]
+        products = [ [*d, int(d[QUANTITY]) * float(d[PRICE])]
+                for d in (line[:-1].split(',') for line in file) ]
         budget = sum(p[COST] for p in products)/len(products) * 1.5
-        bargains = [
-                p[NAME]
+        bargains = [ p[NAME]
                 for p in products
                 if p[COST] < budget
                 and (p[GRADE] == 'superior'
-                or p[COLOR] == 'blue')]
-        with open(outfile, 'w') as out:
+                or p[COLOR] == 'blue') ]
+        with open(f'{filename.split(".")[0]}.bargains', 'w') as out:
             out.write('\n'.join(bargains))
 
 if __name__ == '__main__':
--- 05X/find.py
+++ 05X/find.py
@@ -8,6 +8,18 @@ import shutil
 def usage():
   print ('usage: ./find.py [--copy dirname] root file')
 
+def find(root, filename):
+    return [ os.path.join(d, filename)
+            for d, _subds, files in os.walk(root)
+            if filename in files ]
+
+def copy(found, new_dir):
+    path = f'{os.path.join(os.getcwd(), new_dir)}'
+    if not os.path.exists(path):
+        os.mkdir(path)
+    for f in found:
+        shutil.copyfile(f, os.path.join(path, f[1:].replace('/', '_')))
+
 def main():
   if len(sys.argv) < 2: usage()
   if sys.argv[1] == '--copy':
